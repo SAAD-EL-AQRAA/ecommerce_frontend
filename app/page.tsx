7311abba21cd12eabaaee072f6b6ccec50300c62
E-SAAD2005 <saadelaqraa2005@gmail.com>
2026-01-31 16:21:08 +0100

    Update ecommerce

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { client } from '../lib/client';
-import { Product, FooterBanner, HeroBanner } from '../componenet';
+import { Product as ProductComponent, FooterBanner, HeroBanner } from '../componenet';
+import { Product, Banner } from 'ecommerce-sanity/schemaTypes/index';
 
 const Home = async () => {
   const productsQuery = '*[_type == "product"]';
   const bannerQuery = '*[_type == "banner"]';
 
-  const products = await client.fetch(productsQuery);
-  const bannerData = await client.fetch(bannerQuery);
+  const products: Product[] = await client.fetch(productsQuery);
+  const bannerData: Banner[] = await client.fetch(bannerQuery);
 
   return (
     <div>
-      <HeroBanner heroBanner={bannerData?.length && bannerData[0]} />
+      <HeroBanner heroBanner={bannerData?.length ? bannerData[0] : null} />
 
       <div className="products-heading">
         <h2>Best Seller Products</h2>
@@ -19,12 +20,12 @@ const Home = async () => {
       </div>
 
       <div className="products-container">
-        {products?.map((product) => (
-          <Product key={product._id} product={product} />
+        {products?.map((product: Product) => (
+          <ProductComponent key={product._id} product={product} />
         ))}
       </div>
 
-      <FooterBanner footerBanner={bannerData && bannerData[0]} />
+      <FooterBanner footerBanner={bannerData?.[0] || null} />
     </div>
   );
 };
