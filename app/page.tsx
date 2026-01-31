1bdc02df3a4c5e45a2c6ac3a4204fc008a85c21e
E-SAAD2005 <saadelaqraa2005@gmail.com>
2026-01-31 17:23:40 +0100

    Fix Sanity types import

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { client } from '../lib/client';
 import { Product as ProductComponent, FooterBanner, HeroBanner } from '../componenet';
-import { Product, Banner } from 'ecommerce-sanity/schemaTypes/index';
-
+//import { Product, Banner } from 'ecommerce-sanity/schemaTypes/index';
+import { Product, Banner } from '../types/sanity';
 const Home = async () => {
   const productsQuery = '*[_type == "product"]';
   const bannerQuery = '*[_type == "banner"]';
